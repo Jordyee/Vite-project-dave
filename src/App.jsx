3aa7b15ae314554269a7b5c9d01fcb48e36ed037
jordyee <gerungan.dj@gmail.com>
2025-11-04 15:22:17 +0700

    Belajar pake button dengan state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-const App =  () => {
+// komponen adalah fungsi yang return jsx
+// ini div adalah komponen yang return jsx
+import { useState } from "react";
+import HelloWorld from "./HelloWorld";
+
+const App = () => {
+  // let hello = "Hello World"; //Deklarasi variabel biasa
+  const [hello, setHello] = useState("Hello World !!!"); //Deklarasi variable state
+
+  const handleClick = () => {
+    console.log("Button di klik");
+    // hello = "Selamat Datang"; //Cara merubah variabel biasa
+    setHello("Selamat Datang"); //Cara merubah variabel state
+
+    // console.log(hello);
+  };
+
   return (
-    <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
-      hello world 
-    </div>
+    <>
+      <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
+        {hello}
+      </div>
+      <HelloWorld text="Ini adalah Hello World 1" handleClick={handleClick} />
+      <HelloWorld text="Ini adalah Hello World 2" />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 py-4 px-4 rounded-2xl text-white"
+        onClick={handleClick}
+      >
+        Click Me
+      </button>
+    </>
   );
 };
 
